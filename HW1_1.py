3ccd36f7853235787901c2af90af9899ebe6d044
Tony Bailey <baileytj@mail.uc.edu>
2018-10-02 22:25:44 -0400

    Added a lot of comments. I also changed some formatting of the plots.

--- HW1_1.py
+++ HW1_1.py
@@ -10,18 +10,15 @@ import matplotlib.pyplot as plt
     Modified again by Tony Bailey for HW 1
 """
 
-"""
-
-"""
-
 class Neuron:
     """
-    Regular Spiking Neuron:
+    Izhikevich Neuron Model:
     I defined the neuron as a class so that I could easily replicated its
     behavior in the two neuron system.
     """
 
     def __init__(self, a, b, c, d, V):
+        # Assign neuron parameters to its own internal variables
         self.a = a
         self.b = b
         self.c = c
@@ -32,13 +29,15 @@ class Neuron:
         print(self.a,self.b,self.c,self.d,self.V,self.u)
 
     def step(self, I, tau):
+        # Perform the differential equation calculation via Euler's Method
         self.V = self.V + tau * (0.04 * self.V**2 + 5.0 * self.V + 140.0 - self.u + I)
         self.u = self.u + tau * self.a * (self.b* self.V - self.u)
 
+        # If the membrane voltage is greather than 30, generate a spike
         if self.V > 30:
             self.V = self.c
             self.u = self.u + self.d
-            # Return if a spike occured and VV
+            # Return if a spike occured and V
             return (1, 30)
         else:
             return (0, self.V)
@@ -51,24 +50,27 @@ class Neuron:
 def main():
     # Simulation runs for 1000 steps
     steps = 1000
+
+    # Instantiate Regular Spiking Neuron
     N1 = Neuron(0.02, 0.25, -65.0, 6.0, -64.0)
 
-    tau = 0.25
+    tau = 0.25  # time step length
     tspan = np.arange(0,steps+tau,tau) # Arange understeps by tau
 
     T1 = 50  # Time at which the step input rises.
 
     # All of the 81 cases of I to be tested.
     Input = np.linspace(0,20,81)
-    Inplot = [1, 5, 10, 15, 20]
+
+    Inplot = [1, 5, 10, 15, 20]  # list of values of I that will be plotted.
     R = np.zeros(Input.shape) # preallocate R with the same length as I.
 
-    v_plot = []
+    v_plot = []  # Initialize a list for the series to be plotted.
 
     for istep, i in enumerate(Input):
 
         print(istep)
-        spike_counter = 0
+        spike_counter = 0  # Restart spike counter
         VV = np.zeros(tspan.shape) # preallocate VV with the same length as tspan.
 
         N1.reset()  # Reset neuron to initial -64mV for the next trial.
@@ -79,9 +81,12 @@ def main():
             else:
                 I = 0.0
 
+            # Step the neuron forward a time-step.
             (Spike, VV[int(tstep)]) = N1.step(I, tau)
 
             if Spike and t >= 200.0:
+                # If there was a spike and it is the last 800 steps,
+                # Increment the spike counter.
                 spike_counter+=1
 
 
@@ -95,14 +100,13 @@ def main():
 
     # Plot VV figure
     f1 = 10
-
     for n in range(5):
         plt.subplot(5,1,n+1)
         plt.plot(tspan, v_plot[n])
         plt.xlabel('Time Step', fontsize=f1)
         plt.ylim((-90, 40))
         plt.xlim((0, tspan[-1]))
-        plt.ylabel('$V_{mem}$' , fontsize=f1)
+        plt.ylabel('$V$' , fontsize=f1)
         plt.tick_params(labelsize=f1-2)
         plt.title('Regular Spiking: I={}'.format(Inplot[n]), fontsize=f1, fontweight='bold')
 
@@ -120,4 +124,5 @@ def main():
     plt.show()
 
 if __name__ == '__main__':
+    # Run the main function.
     main()
--- HW1_2.py
+++ HW1_2.py
@@ -20,7 +20,7 @@ def main():
     N1 = Neuron(0.02, 0.25, -65.0, 6.0, -64.0)
     N2 = Neuron(0.02, 0.25, -65.0, 6.0, -64.0)
 
-    tau = 0.25
+    tau = 0.25 # time step length
     tspan = np.arange(0,steps+tau,tau) # Note, arange understeps by tau
 
     T1 = 50  # Time at which the step input rises.
@@ -29,16 +29,19 @@ def main():
 
     # All of the 81 cases of I to be tested.
     Input = np.linspace(0,20,81)
-    Inplot = [1, 5, 10, 15, 20]
+
+    Inplot = [1, 5, 10, 15, 20]  # list of values of I that will be plotted.
     R1 = np.zeros(Input.shape) # preallocate R with the same length as I.
     R2 = np.zeros(Input.shape) # preallocate R with the same length as I.
 
+    # Initialize lists for the series to be plotted.
     v_plot1 = []
     v_plot2 = []
 
     for istep, i in enumerate(Input):
 
         print(istep)
+        # Restart spike counters
         spike_counter1 = 0
         spike_counter2 = 0
 
@@ -55,9 +58,13 @@ def main():
             else:
                 Ia = 0.0
 
+            # Step Neuron A.
             (Spike1, VV1[int(tstep)]) = N1.step(Ia, tau)
+            # Take the spike output (1 or 0) and multiply it by w.
             (Spike2, VV2[int(tstep)]) = N2.step(Spike1*w, tau)
 
+            # If either neuron spiked and it is the last 800 time steps,
+            # Increment the correct spike counter.
             if Spike1 and t >= 200.0:
                 spike_counter1 += 1
             if Spike2 and t >= 200.0:
@@ -72,9 +79,8 @@ def main():
         R1[int(istep)]=spike_counter1/800.0
         R2[int(istep)]=spike_counter2/800.0
 
-    # Plot VV figure 1
+    # Plot VV figure for N_A.
     f1 = 10
-
     for n in range(5):
         plt.subplot(5,1,n+1)
         plt.plot(tspan, v_plot1[n])
@@ -86,35 +92,34 @@ def main():
         plt.title('Regular Spiking: $I_A$={}'.format(Inplot[n]), fontsize=f1, fontweight='bold')
 
     plt.subplots_adjust(left=0.075, bottom=0.05, right=0.95, top=0.925, wspace=5, hspace=0.75)
-    plt.suptitle('HW1 pt. 2.1: Neuron A\'s Response Over Time', fontsize=f1+2)
+    plt.suptitle('HW1 pt. 2.1: $N_A$\'s Response Over Time', fontsize=f1+2)
     plt.show()
 
-    # Plot VV figure 2
+    # Plot VV figure for N_B
     for n in range(5):
         plt.subplot(5,1,n+1)
         plt.plot(tspan, v_plot2[n])
         plt.xlabel('Time Step', fontsize=f1)
         plt.ylim((-90, 40))
         plt.xlim((0, tspan[-1]))
-        plt.ylabel('$V_{mem}$' , fontsize=f1)
+        plt.ylabel('$V$' , fontsize=f1)
         plt.tick_params(labelsize=f1-2)
         plt.title('Regular Spiking: $I_A$={}'.format(Inplot[n]), fontsize=f1, fontweight='bold')
 
     plt.subplots_adjust(left=0.075, bottom=0.05, right=0.95, top=0.925, wspace=5, hspace=0.75)
-    plt.suptitle('HW1 pt. 2.2: Neuron B\'s Response Over Time', fontsize=f1+2)
+    plt.suptitle('HW1 pt. 2.2: $N_B$\'s Response Over Time', fontsize=f1+2)
     plt.show()
 
 
-    # Plot R figure
+    # Plot R_N_B vs R_N_A figure
     plt.scatter(R1, R2)
-    #plt.plot(R1, R2)
-    plt.grid(True)
-    plt.xlabel('Neuron A\'s Mean-Spike Rate: R-$I_A$', fontsize=f1)
+    plt.xlabel('$N_A$\'s Mean Spike Rate: R-$N_A$', fontsize=f1)
     plt.ylim((-0.001, max(R2)+0.001))
     plt.xlim((-0.001, max(R1)+0.001))
-    plt.ylabel('Neuron B\'s Mean-Spike Rate: R-$I_B$' , fontsize=f1)
-    plt.tick_params(labelsize=f1-2)
-    plt.title('HW1 pt. 2.3: Mean Spike Rate of Neuron B vs. Mean Spike Rate of Neuron A'.format(Inplot[n]), fontsize=f1, fontweight='bold')
+    plt.ylabel('$N_B$\'s Mean Spike Rate: R-$N_B$' , fontsize=f1)
+    plt.tick_params(labelsize=f1)
+    plt.title('HW1 pt. 2.3: Mean Spike Rate of $N_B$ vs. Mean Spike Rate of $N_A$'.format(Inplot[n]),
+                fontsize=f1+2)
 
     plt.show()
 
